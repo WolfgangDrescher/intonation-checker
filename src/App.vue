10b35e0df24f1739188f346973b32dd1415b0150
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-02-28 11:59:43 +0100

    Setup verovio

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <script setup>
 import IntonationChecker from './components/IntonationChecker.vue';
+import { createWorkerVerovioToolkit } from 'vue-verovio-canvas';
+import createVerovioWorker from './verovio-worker.js?worker';
+const toolkit = createWorkerVerovioToolkit(createVerovioWorker());
 </script>
 
 <template>
-    <IntonationChecker />
+    <IntonationChecker
+        :toolkit="toolkit"
+        score-url="Beethoven op 74_2 T24-49.mei"
+        title="Beethoven Op. 74, 2. Satz, Takt 24-49"
+        description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua."
+    />
 </template>
--- src/components/IntonationChecker.vue
+++ src/components/IntonationChecker.vue
@@ -1,3 +1,33 @@
+<script setup>
+import 'vue-verovio-canvas/style.css';
+import { VerovioCanvas } from 'vue-verovio-canvas';
+
+defineProps({
+    toolkit: Object,
+    scoreUrl: String,
+    title: String,
+    description: String,
+});
+
+function scoreClickHandler(event) {
+    console.log(event);
+}
+</script>
+
 <template>
-    <div>intonation-checker</div>
+    <div class="lg:w-full lg:h-full lg:flex overflow-hidden">
+        <div class="flex-grow" @click="scoreClickHandler">
+            <VerovioCanvas :toolkit="toolkit" :url="scoreUrl" :pageMargin="50" />
+        </div>
+        <div class="flex-grow-0 flex-shrink-0 lg:w-[500px] shado border-l">
+            <div class="p-4 bg-gray-100 border-b">
+                <div class="text-xl font-bold">
+                    {{ title }}
+                </div>
+                <p v-if="description" class="mt-4 mb-0">
+                    {{ description }}
+                </p>
+            </div>
+        </div>
+    </div>
 </template>
