431d6ab6d5b27e05f60b3beab6f355c0c374c373
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-02-28 22:32:40 +0100

    Add ErrorList components

---
+++ src/components/ErrorList.vue
@@ -0,0 +1,18 @@
+<script setup>
+import ErrorListItem from './ErrorListItem.vue';
+
+defineProps({
+    errors: Array,
+});
+</script>
+
+<template>
+    <div>
+        <div class="mb-4">
+            Total {{ errors?.length }}
+        </div>
+        <div class="flex flex-col gap-4">
+            <ErrorListItem v-for="(error, index) in errors" :key="index" :error="error" />
+        </div>
+    </div>
+</template>
---
+++ src/components/ErrorListItem.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineProps({
+    error: Object,
+});
+</script>
+
+<template>
+    <div class="p-3 border shadow rounded">
+        {{ error }}
+    </div>
+</template>
