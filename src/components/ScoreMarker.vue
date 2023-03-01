33b1c4a164d73d663adf537bf3b67545c20659a8
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-03-01 22:30:22 +0100

    Add ScoreMarker component

---
+++ src/components/ScoreMarker.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { reactive } from 'vue';
+
+const props = defineProps({
+    marker: Object,
+    elem: SVGGElement,
+    parent: HTMLElement,
+});
+
+const elemmRect = props.elem.getBoundingClientRect();
+const parentRect = props.parent.getBoundingClientRect();
+
+const markerSize = 50;
+
+const position = reactive({
+    width: `${markerSize}px`,
+    height: `${markerSize}px`,
+    left: `${elemmRect.x + elemmRect.width / 2 - parentRect.x}px`,
+    top: `${elemmRect.y + elemmRect.height / 2 - parentRect.y}px`,
+});
+</script>
+
+<template>
+    <div
+        :style="position"
+        class="rounded-full bg-opacity-50 absolute -translate-x-1/2 -translate-y-1/2"
+        :class="marker.validated ? (marker.isCorrect ? 'bg-green-500' : 'bg-red-500') : 'bg-yellow-500'"
+    ></div>
+</template>
