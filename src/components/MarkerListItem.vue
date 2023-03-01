d7f94520dfe0f41ac0101f5b6d4983c108852896
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-03-01 01:00:38 +0100

    Show if selected marker is correct

--- src/components/MarkerListItem.vue
+++ src/components/MarkerListItem.vue
@@ -1,12 +1,25 @@
 <script setup>
+import { useMarkersStore } from '../stores/markers.js';
+import { storeToRefs } from 'pinia';
+import { computed } from 'vue';
 import Button from './Button.vue';
 import { Icon } from '@iconify/vue';
 
-defineProps({
+const props = defineProps({
     marker: Object,
 });
 
 const emit = defineEmits(['audioSeek', 'audioSeekFactor']);
+
+const store = useMarkersStore();
+const { markers } = storeToRefs(store);
+
+const isCorrect = computed(() => {
+    return markers.value.reduce((accumulator, item) => {
+        accumulator.push(...item.noteIds);
+        return accumulator;
+    }, []).some(id => props.marker.noteIds.includes(id));
+});
 
 function playAudioListener()  {
     if (props.marker.time) {
@@ -18,7 +31,7 @@ function playAudioListener()  {
 </script>
 
 <template>
-    <div class="p-3 border shadow rounded">
+    <div class="p-3 border shadow rounded" :class="isCorrect ? 'bg-green-300' : 'bg-red-300'">
         {{ marker }}
         <Button @click="playAudioListener">
             <Icon icon="heroicons-solid:play" width="1.5rem" />
