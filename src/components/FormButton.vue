790457a14ece26f4d2f6bc852aea23c683fe9207
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-03-08 01:18:43 +0100

    Add active prop for buttons

--- src/components/FormButton.vue
+++ src/components/FormButton.vue
@@ -1,5 +1,11 @@
+<script setup>
+defineProps({
+    active: Boolean,
+});
+</script>
+
 <template>
-    <button class="form-group">
+    <button class="form-group" :class="{active}">
         <slot></slot>
     </button>
 </template>
@@ -7,5 +13,9 @@
 <style scoped>
 .form-group {
     @apply p-1 bg-gray-200 rounded;
+}
+
+.form-group.active {
+    @apply bg-red-500 text-white;
 }
 </style>
