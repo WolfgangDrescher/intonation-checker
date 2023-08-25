cd12d4c3f52224bb346562b6cd0b04045180d974
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-08-25 08:44:27 +0200

    Remove unused postcss-prefix-selector

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import postcssPrefixSelector from 'postcss-prefix-selector';
 
 // https://vitejs.dev/config/
 export default defineConfig({
